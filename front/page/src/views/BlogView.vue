63a970a948cd5cf90e4e5a51a79d30babb8bff89
SMLMKZR <miki_zero@qq.com>
2024-09-18 20:21:51 +0800

    博客页面布局完成

--- front/page/src/views/BlogView.vue
+++ front/page/src/views/BlogView.vue
@@ -9,7 +9,14 @@
           博主信息
         </el-aside>
         <el-main>
-          <v-md-editor :model-value="blogContent" mode="preview"></v-md-editor>
+          <el-container>
+            <el-main>
+              <v-md-editor :model-value="blogContent" mode="preview"></v-md-editor>
+            </el-main>
+            <el-footer>
+              评论区
+            </el-footer>
+          </el-container>
         </el-main>
       </el-container>
     </el-main>
@@ -26,7 +33,24 @@ export default {
   },
   setup() {
     const data = {
-      blogContent: '## 标题'
+      blogContent: '# 标题\n'
+        + '## 二级标题\n'
+        + '### 三级标题\n'
+        + '\n'
+        + '#### 四级标题\n'
+        + '##### 五级标题\n'
+        + '###### 六级标题\n'
+        + '\n'
+        + '**加粗**\n'
+        + '\n'
+        + '*斜体*\n'
+        + '\n'
+        + '~~删除线~~\n'
+        + '\n'
+        + '> 引用\n'
+        + '\n'
+        + '`代码`\n',
+
     };
 
     return data;
@@ -37,5 +61,27 @@ export default {
 <style scoped>
 .el-header {
   padding: 0px;
+}
+
+/**加上背景,方便观察 */
+.el-main {
+  background-color: #E9EEF3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+.el-aside {
+  background-color: #D3DCE6;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+.el-footer {
+  background-color: #B3C0D1;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
 }
 </style>
